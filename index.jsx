e5bf4ad0174ac7f09162351e501186aa0f9413fb
Yunsup Jung <yuun@MacBookPro.lan>
2026-01-26 22:21:14 -0800

    feat: add routing and about page

--- index.jsx
+++ index.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Page from './src/Page';
+import About from './src/About';
+import { Navbar7 } from './components/Navbar7';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Page />
+    <Router>
+      <Navbar7 />
+      <Routes>
+        <Route path="/" element={<Page />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
---
+++ src/About.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div>
+      <h1 className="text-4xl">About Us</h1>
+      <p>This is the about page.</p>
+    </div>
+  );
+}
